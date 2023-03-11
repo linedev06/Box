ade1f732ba29ec24ef2e6d4b35ee3b99ebf84bfd
linedev06 <vitorafelismino@gmail.com>
2023-03-10 21:27:18 -0300

    exercicio box resolvido

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("box");
+Console.WriteLine("+-------+");
+Console.WriteLine("| Vitor |");
+Console.WriteLine("+-------+");
